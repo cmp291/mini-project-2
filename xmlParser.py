35618f4966fb16ff0b790cc810aa7e54516b2862
Iyunoluwa Lapite <lapite@ualberta.ca>
2018-11-21 23:46:54 -0700

    phase 1
    done except issues with terms.txt

--- xmlParser.py
+++ xmlParser.py
@@ -5,19 +5,25 @@ def readTI(ti,TIs):
         tiTerms = []
         ti = ti.split()
         for ch in ti:
-                if ((len(ch) > 2) and (re.match('\w',ch) or ch == "_" or ch == "-")):
+                sch = re.search('(.*)&#(\d*);',ch)
+                if sch:  
+                        ch = sch.group(1)                   
+                if (len(ch) > 2) and (re.match('\w',ch) or re.match('-',ch)):
                         tiTerms.append(ch)
         TIs.append(tiTerms)
- 
+
 def readDESC(desc,DESCs):
         descTerms = []
         desc = desc.split()
         for ch in desc:
-                if ((len(ch) > 2) and (ch.isalnum() or ch == "_" or ch == "-")):
-                        descTerms.append(ch)
+                sch = re.search('&#(\d*);',ch)
+                if sch:  
+                        ch = sch.group(1)  
+                if (len(ch) > 2) and (re.match('\w',ch) or re.match('-',ch)):
+                        descTerms.append(ch)               
         DESCs.append(descTerms)
 
-def parse(line,terms,IDs,Dates,LOCs,CATs,TIs,DESCs,Prices):
+def parse(line,IDs,Dates,LOCs,CATs,TIs,DESCs,Prices):
         searchObj = re.search( '(.*)<aid>(.*)</aid>', line)
         if searchObj:
                 IDs.append(searchObj.group(2))
@@ -48,6 +54,17 @@ def writeTerms(terms,IDs,TIs,DESCs):
                 for trm in allTerms[i]:
                         terms.write(trm.lower() + ":" +IDs[i] + "\n")
 
+def writePDates(pdates,IDs,Dates,CATs,LOCs):
+        for i in range(len(IDs)):
+                pdates.write(Dates[i] + ":" +IDs[i] + "," + CATs[i] + "," + LOCs[i] + "\n")
+
+def writePrices(prices,IDs,Prices,CATs,LOCs):
+        for i in range(len(IDs)):
+                prices.write(Prices[i] + ":" +IDs[i] + "," + CATs[i] + "," + LOCs[i] + "\n")
+
+def writeAds(ads,IDs,xmlAds):
+        for i in range(len(IDs)):
+                ads.write(IDs[i]+ ":" + xmlAds[i] + "\n")
 
 def main():
         file = open(sys.argv[1],"r")
@@ -55,7 +72,10 @@ def main():
         file.close()
         contents = contents.split("\n")
         terms = open("terms.txt", "w")
-        ads = []
+        pdates = open("pdates.txt", "w")
+        prices = open("prices.txt","w")
+        ads = open("ads.txt","w")
+        xmlAds = []
         IDs = []
         Dates = []
         LOCs = []
@@ -65,10 +85,16 @@ def main():
         Prices = []
         for line in contents:
                 if re.findall(r'^<ad>',line, flags=0):
-                        ads.append(line)
-                        parse(line,terms,IDs,Dates,LOCs,CATs,TIs,DESCs,Prices) 
+                        xmlAds.append(line)
+                        parse(line,IDs,Dates,LOCs,CATs,TIs,DESCs,Prices) 
         writeTerms(terms,IDs,TIs,DESCs)
+        writePDates(pdates,IDs,Dates,CATs,LOCs)
+        writePrices(prices,IDs,Prices,CATs,LOCs)
+        writeAds(ads,IDs,xmlAds)
         terms.close()
+        pdates.close()
+        prices.close()
+        ads.close()
 
 if __name__ == "__main__":
         main()
